edb0594f7c2f7482b34dbc9305aec2fece511fd4
Anzelah <jepkoechanzellah@gmail.com>
2026-02-19 10:48:38 +0300

    fix the authmiddleware function and add comments

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,26 +1,27 @@
 require("dotenv").config()
 const jwt = require("jsonwebtoken")
 
-// Get the header containing token
+// Verifies the token and attaches userId inside token to req
 function authMiddleware (req, res, next) {
     const authHeader = req.headers.authorization
-}
 
-if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(501).json({ error: "Unauthorized!"})
-}
-const token = authHeader.split(" ")[1]
+    // retrieve the token
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(501).json({ error: "Unauthorized!"})
+    }
+    const token = authHeader.split(" ")[1]
 
-// verify if the token is valid
-try {
-    const secret = process.env.JWT_SECRET
-    const decoded = jwt.verify(token, secret)
+    // verify if the token is valid
+    try {
+        const secret = process.env.JWT_SECRET
+        const decoded = jwt.verify(token, secret)
 
-    // attaches userId inside token to request so the user uses the token for future requests
-    req.userId = decoded.userId
-    next()
-} catch(error) {
-    res.status(401).json({ error: "Invalid token!"})
+        // extract the original user ID inside the token for further queries
+        req.userId = decoded.userId
+        next()
+    } catch(error) {
+        res.status(401).json({ error: "Invalid token!"})
+    }
 }
 
 module.exports = authMiddleware;
