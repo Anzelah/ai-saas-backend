197c9fdbe29000e91bae37443e9e83d60549e0f8
Anzelah <jepkoechanzellah@gmail.com>
2026-02-20 18:31:53 +0300

    create /ai route for generating responses

--- src/routes/ai.js
+++ src/routes/ai.js
@@ -4,3 +4,56 @@ const authMiddleware = require("../middleware/auth")
 
 const prisma = new PrismaClient()
 const router = express.Router()
+
+router.post("/generate", authMiddleware, async (req, res) => {
+    try { 
+        // Check the user has sent a prompt
+        const { prompt } = req.body
+        if(!prompt) {
+            return res.json(400).res.json({ error: "Prompt is required"})
+        }
+        
+        // find the user and ssubscriptions from db
+        const user = await prisma.user.findUnique({
+            where: { id: req.userId },
+            include: { subscription: true },
+        })
+        if (!user) {
+            res.status(400).json({ error: "User not found"})
+        }
+
+        // fetch the user's subscription
+        const subscription = user.subscription
+        if (!subscription) {
+            res.status(400).json({ error: "Subscription not found"})
+        }
+
+        // check the credits available
+        const credits = subscription.credits
+        if (credits <= 0) {
+            res.status(403).json({ error: "No credits remaining"})
+        }
+
+        // User has subscription with enough credits, now backend can generate a response
+        // temporary fake response for testing
+        const aiResponse = `Ai response to: ${prompt}`
+
+        // Save request in db for history + record for product usage
+        await prisma.aIRequest.create({
+            data: { prompt,
+                response: aiResponse,
+                userId: user.id,
+            },
+        })
+
+        // backend deducts credits
+
+
+
+
+    } catch(error) {
+
+    }
+})
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5000
 
 const authRoutes = require("./routes/auth")
 const userRoutes = require("./routes/user")
+const aiRoutes = require("./routes/ai")
 
 const app = express()
 
@@ -12,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use("/auth", authRoutes)
 app.use("/user", userRoutes)
+app.use("/ai", aiRoutes)
 
 
 app.get("/", (req, res) => {
