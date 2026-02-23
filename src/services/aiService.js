f1b08d983611dd1619f2e07cafe72daf680d92d2
Anzelah <jepkoechanzellah@gmail.com>
2026-02-23 22:14:16 +0300

    create a function to call the openai api and generate a text response to be returned to route

--- src/services/aiService.js
+++ src/services/aiService.js
@@ -1,3 +1,23 @@
+require("dotenv").config()
 const OpenAI = require("openai")
 
-const openai = new OpenAI()
+// create an openai client and authenticate it using my secret api key
+const apiKey = process.env.OPENAI_API_KEY
+const openai = new OpenAI({ apiKey })
+
+// Call openai apito generate a response based on the prompt provided
+async function generateAIResponse(prompt) {
+    const completion  = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+            { role: "system", content: "You are an expert entepreneur" },
+            { role: "user", content: prompt }
+        ],
+    })
+
+    // extract and return the response to the route
+    const openaiResponse = completion.choices[0].message.content
+    return openaiResponse
+}
+
+module.exports = { generateAIResponse }
