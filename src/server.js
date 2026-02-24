4ef0a7fc1483f99171c8881d9c2abf881d15ec6b
Anzelah <jepkoechanzellah@gmail.com>
2026-02-24 17:18:38 +0300

    apply basic rate limiter to protect all routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
+const rateLimit = require("express-rate-limit")
 const PORT = process.env.PORT || 5000
 
 const authRoutes = require("./routes/auth")
@@ -8,9 +9,13 @@ const userRoutes = require("./routes/user")
 const aiRoutes = require("./routes/ai")
 
 const app = express()
-
 app.use(cors())
 app.use(express.json())
+
+// rate limit all routes to 100 requests per 15 minutes
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, limit: 100 })
+app.use(limiter)
+
 app.use("/auth", authRoutes)
 app.use("/user", userRoutes)
 app.use("/ai", aiRoutes)
