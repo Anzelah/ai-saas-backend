b11c267267858f4fc0e806300f309d042c521f4b
Anzelah <jepkoechanzellah@gmail.com>
2026-03-02 21:16:09 +0300

    completed signup schema validation

---
+++ src/validators/authSchemas.js
@@ -0,0 +1,11 @@
+const { z } = require("zod")
+
+// signup validation
+const signupSchema = z.object({
+    email: z.email({ message: "Invalid email format"}),
+    password: z.string()
+    .min(8, { message: "Password must be at least 8 characters" })
+    .max(100, { message: "Password too long" })
+    .refine((val) => /[A-Z]/.test(val), { message: "Password must contain at least one uppercase letter" })
+    .refine((val) => /[!@#$%^&*()?:{}|<>]/.test(val), { message: "Password must contain at least one special character: !@#$%^&*()?:{}|<></>" })
+})
