a50a402c921c39c5ebdea4ab1e78444c98bbaac1
Anzelah <jepkoechanzellah@gmail.com>
2026-03-03 17:47:45 +0300

    implemented login input validation

--- src/validators/authSchemas.js
+++ src/validators/authSchemas.js
@@ -9,3 +9,11 @@ const signupSchema = z.object({
     .refine((val) => /[A-Z]/.test(val), { message: "Password must contain at least one uppercase letter" })
     .refine((val) => /[!@#$%^&*()?:{}|<>]/.test(val), { message: "Password must contain at least one special character: !@#$%^&*()?:{}|<></>" })
 })
+
+// login validation
+const loginSchema = z.object({
+    email: z.email({ message: "Invalid email format" }),
+    password: z.string().min(8, { message: "Password can't be less than 8 characters" })
+})
+
+module.exports = { signupSchema, loginSchema }
