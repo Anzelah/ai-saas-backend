c5ab2263d3b9f5d0fbe226bb47aa2a0c5f7832f3
Anzelah <jepkoechanzellah@gmail.com>
2026-03-03 17:52:26 +0300

    ai generate route propmt validation implemented

---
+++ src/validators/aiSchemas.js
@@ -0,0 +1,10 @@
+const { z } = require("zod")
+
+// Prompt validation
+const generateSchema = z.object({
+    prompt: z.string()
+    .min(5, { message: "Prompt must be atleast 5 characters" })
+    .max(2000, { message: "Prompt too long" })
+})
+
+// Route params validation
