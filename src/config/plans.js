3f76a4b53067e1f3c0070e8124d0ef5acbb1ff2c
Anzelah <jepkoechanzellah@gmail.com>
2026-03-06 22:54:27 +0300

    define plans in backend

---
+++ src/config/plans.js
@@ -0,0 +1,19 @@
+const plans = {
+    starter: {
+        name: "Started Plan",
+        price: 1000,
+        credits: 100
+    },
+    pro: {
+        name: "Pro Plan",
+        price: 2000,
+        credits: 300
+    },
+    enterprise: {
+        name: "Enterprise Plan",
+        price: 5000,
+        credits: 1000
+    }
+}
+
+module.exports = plans
