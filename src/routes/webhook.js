831cef31b644053057695ccd4248dd90bde8cfd8
Anzelah <jepkoechanzellah@gmail.com>
2026-03-09 14:06:40 +0300

    extract and verify the stripe signature

---
+++ src/routes/webhook.js
@@ -0,0 +1,29 @@
+require("dotenv").config()
+const express = require("express")
+const stripe = require("../lib/stripe")
+const AppError = require("../utils/AppError")
+
+const router = express.Router()
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET
+
+router.post("/webhook", express.raw({ type: "application/json" }), async(req, res) => {
+    // get event data sent by stripe. contains id, type, and related stripe resources under data object
+    let event = req.body
+
+    const sig = req.headers['stripe-signature']
+    // verify the stripe signature against our secret
+    try {
+        stripe.webhooks.constructEvent(
+            req.body,
+            sig,
+            endpointSecret
+        )
+    } catch(error) {
+        throw new AppError("Webhook signature verification failed", 500)
+    }
+
+    // if payment had been made, and signature verified, update the db
+    if (event.type === "checkout.session.completed") {
+
+    }
+})
